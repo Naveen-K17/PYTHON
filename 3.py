db09a9006ae2cc99ad5b0c6f5b5aa98a3f3b80c7
Naveen-K17 <naveenkattimani326@gmail.com>
2026-04-26 11:46:20 +0530

    update

---
+++ 3.py
@@ -0,0 +1,15 @@
+# a=int(input("Enter a 1 number:"))
+# b=int(input("Enter a 2 number:"))
+
+# print(type(a))
+# print(type(b))
+
+# print(a+b)
+
+# boy_name=str(input("Enter the boy name: "))
+# girl_name=str(input("Enter the girl name: "))
+
+# print(boy_name+ " loves " +girl_name)
+# print(f"{boy_name}  loves {girl_name}")
+
+print(abs(20-50))
